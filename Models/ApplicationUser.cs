8267c583d1478d6d7e70dd348a770c68b15ebe85
RamazanKatran <ramazan.katran@ogr.sakarya.edu.tr>
2025-12-11 22:41:12 +0300

    Üçüncü Gün

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebProjeGym.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public MemberProfile MemberProfile { get; set; }
+    }
+
+    public class MemberProfile
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+
+        [Range(100, 250)]
+        public int? HeightCm { get; set; }
+
+        [Range(30, 250)]
+        public float? WeightKg { get; set; }
+
+        [StringLength(200)]
+        public string Goal { get; set; } // "kilo verme", "kas geliştirme" gibi
+    }
+}

1c5ac8d34fb9c02959ce5cdf2a34a9a92024d36a
RamazanKatran <ramazan.katran@ogr.sakarya.edu.tr>
2025-12-12 20:21:47 +0300

    5.Gün Commiti

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,40 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebProjeGym.Models;
 
 namespace WebProjeGym.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<GymBranch> GymBranches { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<TrainerService> TrainerServices { get; set; }
+        public DbSet<TrainerAvailability> TrainerAvailabilities { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<MemberProfile> MemberProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TrainerService>()
+                .HasKey(ts => new { ts.TrainerId, ts.ServiceId });
+
+            builder.Entity<TrainerService>()
+                .HasOne(ts => ts.Trainer)
+                .WithMany(t => t.TrainerServices)
+                .HasForeignKey(ts => ts.TrainerId);
+
+            builder.Entity<TrainerService>()
+                .HasOne(ts => ts.Service)
+                .WithMany(s => s.TrainerServices)
+                .HasForeignKey(ts => ts.ServiceId);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using WebProjeGym.Data;
+using WebProjeGym.Models;
+using Microsoft.AspNetCore.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +13,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
